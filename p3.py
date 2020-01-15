f8d8f96ec4233246304450520b12c7b80d373e10
Rohit Bhati <Rbhati@bestpeers.com>
2020-01-15 16:01:36 +0530

    Solution of 4rd problem of given Python Programming Exercise.

---
+++ p3.py
@@ -0,0 +1,27 @@
+"""Question 4: Write a program which accepts a sequence of comma-separated numbers from console
+and generate a list and a tuple which contains every number."""
+
+#TUDO
+seq =  input("Write sequence of numbers:")
+import pdb;pdb.set_trace()
+l = []
+t = ()
+for x in seq:
+	a = l.extend(str(x))
+# seq=input()
+# lis=[]
+# import pdb;pdb.set_trace()
+
+# var=''
+# for x in seq:
+# 	if x ==',':
+# 		lis.append(var)
+# 		var=''
+# 		continue
+		
+
+# 	var=var+x
+
+# lis.append(var)
+# print(lis)
+# print(tuple(lis))
